301b9137a236823999e7caeb46610516153a14b2
Dan Gove <dnlgove2@gmail.com>
2022-12-06 02:58:07 -0500

    Added post edit and delete functionality

--- ucug_forum/urls.py
+++ ucug_forum/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path("forum/<int:id>/", views.forum_feed),
     path("edit_forum/", views.edit_forum, name="edit_forum"),
     path("create_post/", views.create_post, name="create_post"),
+    path("edit_post/", views.edit_post, name="edit_post"),
+    path("delete_post/", views.delete_post, name="delete_post"),
     path("get_posts/", views.get_posts_view, name="get_posts"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
